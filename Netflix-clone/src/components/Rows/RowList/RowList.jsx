5fc4016fc8af4a5ccaa7dbaf41b7bbbf50dfbe77
tsehayg <tsehayg684@gmail.com>
2025-05-13 19:23:08 -0400

    Banner

--- Netflix-clone/src/components/Rows/RowList/RowList.jsx
+++ Netflix-clone/src/components/Rows/RowList/RowList.jsx
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import Row from  "../Row/Row"
-// import requests from '../../../utils/requests'
-// const RowList = () => {
-// return (
-//     <>
-//     <Row
-//     title="NETFLIX ORIGINALS"
-//     fetchUrl= {requests.fetchNetflixOriginals}
-//     isLargeRow={true}
-//     />
-//     <Row title= 'Trending Now' fetchUrl={requests.fetchTrending} />
-//     <Row title= 'Top Rated' fetchUrl={requests.fetchTopRatedMovies} />
-//     <Row title= 'Comedy' fetchUrl={requests.fetchComedyMovies} />
-//     <Row title= 'Action movies' fetchUrl={requests.fetchActionMovies} />
-//     <Row title= 'Horror movies' fetchUrl={requests.fetchHorrorMovies} />
-//       <Row title= 'Romance' fetchUrl={requests.fetchRomanceMovies} />
-//       <Row title= 'Documentaries' fetchUrl={requests.fetchDocumentaries} />
-//    </>
-//   )
-// }
-
-// export default RowList
 import React from 'react'
 
 import requests from '../../../utils/requests';
